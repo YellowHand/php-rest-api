5ed52cea16887b0e9ea4542ed5b482e8a3e3a7b0
Pawel <ypawelborkowski@gmail.com>
2020-01-23 22:12:31 +0100

    false-reg

--- database/migrations/2020_01_23_195605_create_notes_table.php
+++ database/migrations/2020_01_23_195605_create_notes_table.php
@@ -16,8 +16,6 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('text');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
        });
     }
 
