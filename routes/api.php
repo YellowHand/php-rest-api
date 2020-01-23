aa8afd742241c9c2126335d7e3e2e2ce0bba2ead
Pawel <ypawelborkowski@gmail.com>
2020-01-23 22:20:32 +0100

    false-reg

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+Route::get('students', 'ApiController@getAllStudents');
+Route::get('students/{id}', 'ApiController@getStudent');
+Route::post('students', 'ApiController@createStudent');
+Route::put('students/{id}', 'ApiController@updateStudent');
+Route::delete('students/{id}', 'ApiController@deleteStudent');
+
 Route::get('hello', function () {
 
   $result = ['result' => 'OK'];
