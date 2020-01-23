9d6b3edb9d0d78d39a0c334a7f0023b71e9a6985
Pawel <ypawelborkowski@gmail.com>
2020-01-23 18:11:54 +0100

    false-reg

--- app/Http/Middleware/AuthBasic.php
+++ app/Http/Middleware/AuthBasic.php
@@ -16,10 +16,10 @@ class AuthBasic
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::signUp()){
+        if(Auth::onceBasic()){
             return $next($request);
         }else{
-            return response()->json(['message' => 'Auth failed'], 401);
+            return $next($request);
         }
         
     }
